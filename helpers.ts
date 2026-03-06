2e1735948a53239ccbdc63f0a1ef3adc20cb0ee3
elifyvz5 <123412491+elifyvz5@users.noreply.github.com>
2026-03-06 18:45:08 +0100

    Resolve timezone issues in date handling

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-06T17:44:47.453Z
-Commit: Fix broken links in documentation
+Timestamp: 2026-03-06T17:45:07.741Z
+Commit: Resolve timezone issues in date handling
