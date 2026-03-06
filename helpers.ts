f96da3bca4df1fe8478e7f553208ad9aafdeb6e6
elifyvz5 <123412491+elifyvz5@users.noreply.github.com>
2026-03-06 18:45:11 +0100

    Resolve null pointer exception in user service

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-06T17:45:09.623Z
-Commit: Correct typo in configuration file
+Timestamp: 2026-03-06T17:45:10.479Z
+Commit: Resolve null pointer exception in user service
