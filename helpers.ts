d327e04b41811e7eb4f66a597d808fe20dcf194d
elifyvz5 <123412491+elifyvz5@users.noreply.github.com>
2026-03-06 18:44:42 +0100

    Add export functionality for reports

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-06T17:44:35.810Z
-Commit: Fix CSS styling bug on mobile devices
+Timestamp: 2026-03-06T17:44:41.798Z
+Commit: Add export functionality for reports
