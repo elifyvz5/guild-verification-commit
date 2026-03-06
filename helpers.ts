cab089fc7ed9bf7c17636f3718e42ea3d4493204
elifyvz5 <123412491+elifyvz5@users.noreply.github.com>
2026-03-06 18:42:17 +0100

    Add dark mode support

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-06T17:42:16.337Z
-Commit: Resolve null pointer exception in user service
+Timestamp: 2026-03-06T17:42:17.320Z
+Commit: Add dark mode support
