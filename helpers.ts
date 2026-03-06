595f55581a49a5793b37104dd1cdabaa3467c13a
elifyvz5 <123412491+elifyvz5@users.noreply.github.com>
2026-03-06 18:44:35 +0100

    Add multi-language support

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-06T17:42:41.620Z
-Commit: Resolve null pointer exception in user service
+Timestamp: 2026-03-06T17:44:34.586Z
+Commit: Add multi-language support
