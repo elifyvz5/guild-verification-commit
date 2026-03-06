1b602366d32dabe65124221ccaab56ee775f4bcb
elifyvz5 <123412491+elifyvz5@users.noreply.github.com>
2026-03-06 18:45:14 +0100

    Patch API endpoint error handling

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-06T17:45:04.445Z
-Commit: Fix broken links in documentation
+Timestamp: 2026-03-06T17:45:13.546Z
+Commit: Patch API endpoint error handling
