1f260effb6aff8690697586f927319a9f8cfcb9b
elifyvz5 <123412491+elifyvz5@users.noreply.github.com>
2026-03-06 18:45:17 +0100

    Fix CSS styling bug on mobile devices

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-06T17:45:13.546Z
-Commit: Patch API endpoint error handling
+Timestamp: 2026-03-06T17:45:16.592Z
+Commit: Fix CSS styling bug on mobile devices
