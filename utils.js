20f3fb2477189932f1c10d4795dde23b60ca2afb
elifyvz5 <123412491+elifyvz5@users.noreply.github.com>
2026-03-06 18:45:00 +0100

    Add export functionality for reports

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-06T17:44:57.691Z
-Commit: Fix CSS styling bug on mobile devices
+Timestamp: 2026-03-06T17:44:59.791Z
+Commit: Add export functionality for reports
